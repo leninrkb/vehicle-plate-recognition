a0e293ab97e50c4fad5a34a176c9282e0d7c75d9
Lenin Acosta <rkb.lenin@gmail.com>
2023-12-24 00:54:46 -0500

    resize and write dataset

--- data_extraction.py
+++ data_extraction.py
@@ -1,7 +1,10 @@
 import os 
+import shutil
 import cv2 as cv
 
-def readImg(path, data:list, labels:list):
+def loadDataset(path, data:list, labels:list):
+    ''' Cargo el dataset en memoria. Las img estan en gris '''
+    print("loading data set")
     directories = os.listdir(path)
     for dirname in directories:
         tpath = f"{path}/{dirname}"
@@ -10,4 +13,50 @@ def readImg(path, data:list, labels:list):
             imgray = cv.imread(f"{tpath}/{filename}", cv.IMREAD_GRAYSCALE)
             data.append(imgray)
             labels.append(dirname)
-    return (data, labels)
+    print("loading data set - done")
+    
+            
+def resizeDataset(data:list, newsize:int):
+    ''' Hago resize a todo el dataset '''
+    print("resizing dataset")
+    for i in range(len(data)):
+        data[i] = cv.resize(data[i], (newsize, newsize))
+    print("resizing dataset - done")
+        
+def createEmptyDir(path):
+    ''' creo un directorio, si existe lo elimino y lo vuelvo a crear'''
+    print("create empty dir")
+    if os.path.isdir(path):
+        shutil.rmtree(path)
+        os.mkdir(path)
+    else:
+        os.mkdir(path)
+    print("create empty dir - done")
+    
+    
+def writeDataset(path, data:list, labels:list):
+    ''' escribo el dataset en disco en la ruta que se indique '''
+    createEmptyDir(path)
+    print("writting dataset")
+    datalen = len(data)
+    labelslen = len(labels)
+    if not datalen == labelslen:
+        print(f"[ERROR]: inconsistencia. existe {datalen} datos y {labelslen} etiquetas")
+        return
+    number = 1
+    last = labels[0]
+    for i in range(len(data)):
+        current = labels[i]
+        if not last == current:
+            number = 1
+            last = current
+        tpath = f"{path}/{current}"
+        if not os.path.isdir(tpath):
+            os.mkdir(tpath)
+        tpath = f"{tpath}/{number}.png"
+        cv.imwrite(tpath, data[i])
+        number += 1
+    print("writting dataset - done")
+        
+    
+    
--- main.py
+++ main.py
@@ -3,7 +3,12 @@ import numpy as np
 
 data = []
 labels = []
-dx.readImg("./dataset/Fnt", data, labels)
+pathDataset = "./dataset/Fnt"
+pathResizedDataset = "./dataset/new"
+
+dx.loadDataset(pathDataset, data, labels)
+dx.resizeDataset(data, 28)
+dx.writeDataset(pathResizedDataset, data, labels)
 
 print(len(data))
 print(len(labels))
