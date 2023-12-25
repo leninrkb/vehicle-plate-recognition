0b5ee06693810f3c174d40a7efb9d3147f0e8039
Lenin Acosta <rkb.lenin@gmail.com>
2023-12-25 17:12:24 -0500

    readme

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # vehicle registration plate recognition
  vehicle plate recognition with python 
+
+# Steps
+- load raw dataset 
+- read each img as gray channel 
+- resize dataset to (m * m)
+- write resized gray dataset
+- load resized dataset
+- binarize dataset
+- write binarized dataset
+- load binary dataset
+- save data-labels array as binary file to faster load later
+- 
+
+
--- main.py
+++ main.py
@@ -5,23 +5,8 @@ import numpy as np
 
 data = []
 labels = []
-# pathDataset = "./dataset/new"
-# pathDataset = "./dataset/Fnt"
-# pathResizedDataset = "./dataset/new"
 
-# dx.loadDataset(pathDataset, data, labels)
-data = ut.loadData("data")
+data = ut.loadData("binary_data")
 labels = ut.loadData("labels")
-print(f"data type: {type(data[0])}")
-# ut.imshow(data[0])
-# ut.saveData("data", data)
-# ut.saveData("labels", labels)
 
 
-dp.binarizeData(data)
-dx.writeDataset("./dataset/binary", data, labels)
-# dx.resizeDataset(data, 28)
-# dx.writeDataset(pathResizedDataset, data, labels)
-
-print(len(data))
-print(len(labels))
