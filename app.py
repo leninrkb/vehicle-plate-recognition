739715973deade52ce1f3d1f046dcd941ac630af
Lenin Acosta <rkb.lenin@gmail.com>
2024-01-06 23:28:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,89 +1,120 @@
 import flet as fl
 import base64
 
-class Persona():
+class Person():
     def __init__(self) -> None:
-        self.nombre = None
-        self.apellido = None
-        self.placa = None
-        self.modelo_auto = None
-        self.base64_placa = None
-        self.base64_persona = None
+        self.name = None
+        self.surname = None
+        self.plate = None
+        self.model = None
+        self.color = None
+        self.base64_plate = None
+        self.base64_photo = None
 
 class Info(fl.UserControl):
+    def __init__(self, entity:Person):
+        super().__init__()
+        self.entity = entity
+        
     def build(self):
-        column = fl.Column()
-        card = fl.Card()
-        container = fl.Container()
-        
-        row = fl.Row()
-        row.vertical_alignment = fl.CrossAxisAlignment.START
-        
-        info = fl.Container()
-        info.padding = 10
-        # info.bgcolor = fl.colors.AMBER
-        info.content = fl.Column(
-            scroll=fl.ScrollMode.ALWAYS,
-            controls=[
-                fl.TextField(
-                    label="Nombres",
-                ),
-                fl.TextField(
-                    label="Apellidos",
-                ),
-                fl.TextField(
-                    label="Modelo del auto",
-                ),
-                fl.TextField(
-                    label="Color del auto",
-                ),
-            ],
-        )
-        
-        imgs = fl.Container()
-        # imgs.bgcolor = fl.colors.AMBER
-        imgs.content = fl.Column(
-            horizontal_alignment= fl.CrossAxisAlignment.CENTER,
-            controls=[
+        imgs = fl.Column(
+            controls = [
                 fl.Text(
-                    value="FOTO",
-                    style= fl.TextThemeStyle.DISPLAY_SMALL,
-                    weight= fl.FontWeight.BOLD,
+                    value = "FOTO",
+                    style =  fl.TextThemeStyle.DISPLAY_SMALL,
+                    weight =  fl.FontWeight.BOLD,
                 ),
                 fl.Image(
-                    src="./assets/lustre.png",
-                    border_radius=10,
-                    fit= fl.ImageFit.COVER,
-                    width=200,
-                    height=200,
+                    src = "./assets/lustre.png",
+                    border_radius = 10,
+                    fit =  fl.ImageFit.COVER,
+                    width = 200,
                 ),
                 fl.Text(
-                    value="PLACA",
-                    style= fl.TextThemeStyle.DISPLAY_SMALL,
-                    weight= fl.FontWeight.BOLD,
+                    value = "PLACA",
+                    style =  fl.TextThemeStyle.DISPLAY_SMALL,
+                    weight =  fl.FontWeight.BOLD,
                 ),
                 fl.Image(
-                    src="./assets/sample.png",
-                    border_radius=10,
-                    fit= fl.ImageFit.COVER,
-                    width=200,
+                    src = "./assets/sample.png",
+                    border_radius = 10,
+                    fit =  fl.ImageFit.COVER,
+                    width = 200,
                 ),
             ]
         )
         
-        row.controls = [
-            imgs
-            ,info
-        ]
+        self.tf_name = fl.TextField(label = "Nombres")
+        self.tf_name.value = self.entity.name
         
-        container.content = row 
-        container.padding = 10
+        self.tf_surname = fl.TextField(label = "Apellidos")
+        self.tf_surname.value = self.entity.surname
         
-        card.content = container
-        column.controls.append(card)
-        return column
+        self.tf_model = fl.TextField(label = "Modelo")
+        self.tf_model.value = self.entity.model
+        
+        self.tf_color = fl.TextField(label = "Color")
+        self.tf_color.value = self.entity.color
+        
+        info = fl.Column(
+            controls = [
+                self.tf_name
+                ,self.tf_surname
+                ,self.tf_model
+                ,self.tf_color
+            ]
+        )
+        return fl.Column(
+            controls = [
+                fl.Card(
+                    expand = True
+                    ,content = fl.Container(
+                        padding = 10
+                        ,content = fl.Row(
+                            controls = [
+                                imgs, info
+                            ]
+                        )
+                    )
+                )
+            ]
+        )
     
+    def get_info(self) -> Person:
+        self.entity.name = self.tf_name.value
+        return self.entity
+        
+        
+class Create(fl.UserControl):
+    def __init__(self, page:fl.Page, entity:Person) -> None:
+        super().__init__()
+        self.page = page
+        self.info = Info(entity)
     
+    def build(self):
+        options = fl.Card(
+            content = fl.Container(
+                padding = 10
+                ,content = fl.Column(
+                    controls = [
+                        fl.TextButton(
+                            text="Guardar"
+                            ,on_click=self.create
+                        )
+                        ,fl.TextButton("cancelar")
+                    ]
+                )
+            )
+        )
+        return fl.Row(
+            controls=[
+                options, self.info
+            ]
+        )
+    
+    def create(self, e):
+        entity = self.info.get_info()
+        print(entity.name)
     
 class Navigation(fl.UserControl):
     def __init__(self, set_page) -> None:
@@ -92,49 +123,49 @@ class Navigation(fl.UserControl):
     
     def build(self):
         rail = fl.NavigationRail(
-            selected_index=0
-            ,label_type=fl.NavigationRailLabelType.ALL
-            ,destinations=[
+            selected_index = 0
+            ,label_type = fl.NavigationRailLabelType.ALL
+            ,destinations = [
                 fl.NavigationRailDestination(
-                    icon=fl.icons.ADD_CIRCLE_OUTLINE_ROUNDED
-                    ,label="Agregar"
+                    icon = fl.icons.ADD_CIRCLE_OUTLINE_ROUNDED
+                    ,label = "Agregar"
                 ),
                 fl.NavigationRailDestination(
-                    icon=fl.icons.LIST_ROUNDED
-                    ,label="Todos",
+                    icon = fl.icons.LIST_ROUNDED
+                    ,label = "Todos",
                 ),
                 fl.NavigationRailDestination(
-                    icon=fl.icons.REMOVE_RED_EYE_OUTLINED
-                    ,label_content=fl.Text("Reconocer")
+                    icon = fl.icons.REMOVE_RED_EYE_OUTLINED
+                    ,label_content = fl.Text("Reconocer")
                 ),
             ]
-            ,on_change=lambda e: self.set_page(e.control.selected_index)
+            ,on_change = lambda e: self.set_page(e.control.selected_index)
         )
         return rail
 
 def main(page:fl.Page):
     page.title = "Placas"
-    page.theme_mode = fl.ThemeMode.LIGHT
-    page.theme = fl.Theme(color_scheme_seed=fl.colors.LIGHT_BLUE_ACCENT)
+    page.theme_mode = fl.ThemeMode.SYSTEM
+    page.theme = fl.Theme(color_scheme_seed = fl.colors.CYAN)
     def set_page(index):
         row.controls[2] = pages[index]
         page.update()
-    
-    page1 = Info()
-    page2 = fl.Text("page two")
-    page3 = Info()
+    entity = Person()
+    entity.name = "Sana Sunomiya"
+    page1 = Create(page, entity)
+    page2 = Info(entity)
+    page3 = Info(entity)
     pages = [page1, page2, page3]
     rail = Navigation(set_page)
     content = pages[0]
-    
     row = fl.Row(
-        expand=True
-        ,controls=[
+        expand = True
+        ,controls = [
             rail
-            ,fl.VerticalDivider(width=1)
+            ,fl.VerticalDivider(width = 1)
             ,content
         ]
     )
     page.add(row)
 
-fl.app(target=main, assets_dir="assets")
+fl.app(target = main, assets_dir = "assets")
